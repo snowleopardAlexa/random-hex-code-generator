bca9f8f778465fc21ca3f66f209f6c4711963ed7
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-03 18:35:46 -0400

    add js dom

---
+++ script.js
@@ -0,0 +1,7 @@
+let outputColor = document.querySelector("#output-color span");
+let output = document.getElementById("output");
+let genBtn = document.getElementById("gen-btn");
+let copyBtn = document.getElementById("copy-btn");
+let customAlert = document.querySelector(".custom-alert");
+let hexString = "0123456789abcdef";
+
